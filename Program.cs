11191ddfd354c18cc4bbf1eb0625ec7515e842b9
ezekielenns <ezekiel.enns@protonmail.com>
2024-03-19 12:26:31 -0300

    close

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
 ﻿//TODO connect to neovim rpc and controll some features with it
+
+
+using NeovimClient;
+var nvim = new NeovimClient<NeovimTcp>(new NeovimTcp("127.0.0.1",6666));
+Console.WriteLine("Starting ");
+nvim.Init();
+//nvim.Action("echo", "REEE");
+var test = nvim.GetApiInfo();
+Console.WriteLine("done?");
+Console.WriteLine(test.ToString());
+nvim.Dispose();
